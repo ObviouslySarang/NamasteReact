d38984ce4b34dfaf1a512b4cd8b19e306055b225
Sarang Dixit <2003krishnadixit@gmail.com>
2024-05-27 06:11:04 +0530

    creating html siblings

--- tut2/App.js
+++ tut2/App.js
@@ -10,20 +10,20 @@ const head = React.createElement(
         </h1>
     </div>
 </div> */
-    const parent = React.createElement(
+    const parent = React.createElement(//   <<----this is not a html tag it is a REACT ELEMENT or REACT OBJECT
         "div",//name of the element you are creating  
         {id:"parent"},// Attributes if you want to give any 
         React.createElement( // this represents the child of the element {this can further be nested }
             "div",
-            {id:"child"},
-            React.createElement(
-                "h1",
-                {},
-                "the nested structure is created"
-            )
-        
+            {id:"child"},[
+            //React.createElement("h1",{},"the nested structure is created")
+            //insted of giving only one child you can give an ARRAY of children if you want to create siblings for eg :
+            React.createElement("h1",{},"this is h1"),
+            React.createElement("h2",{},"this is h2"),
+            React.createElement("h3",{},"this is h3")]
         )
     
     )
     root.render(parent);
+    // this is where the react element gets converted to a html <tag> 
 
