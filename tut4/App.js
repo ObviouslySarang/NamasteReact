1ff34dd92ad211d8be0122575136f1be1a70f094
Sarang Dixit <2003krishnadixit@gmail.com>
2024-06-04 04:25:49 +0530

    added props functionality to the rescard

--- tut4/App.js
+++ tut4/App.js
@@ -31,10 +31,11 @@ const Header = () => {
   );
 };
 
-const Rescard = ()=>(
+const Rescard = (props)=>(
   <div className="rescard">
     <img alt="restraunt-img" className="res-img" src="https://b.zmtcdn.com/data/pictures/3/19733663/6671b708c3454a79a31cc44f7454f44b_featured_v2.jpg?fit=around%7C108%3A108&crop=108%3A108%3B%2A%2C%2A"></img>
-    <center><h3>Zauk</h3></center>
+    
+    <center><h3>{props.name}</h3></center>
   </div>
 );
 
@@ -42,11 +43,11 @@ const Body = ()=> (
   <div className="body">
     <div className="search">Search</div>
     <div className="resContainer">
-        <Rescard/>
-        <Rescard/>
-        <Rescard/>
-        <Rescard/>
-        <Rescard/>
+        <Rescard name="ZAUK"/>
+        <Rescard name="mera_pizza"/>
+        <Rescard name="D wood"/>
+        <Rescard name="Royal Kitchen"/>
+      
     </div>
   </div>
 );
